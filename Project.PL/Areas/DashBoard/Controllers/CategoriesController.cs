7ec688d3cb2410a990f61d46911f96539dd968df
Anas <zarzouranas@gmail.com>
2024-10-03 16:48:51 +0300

    Add project files.

---
+++ Project.PL/Areas/DashBoard/Controllers/CategoriesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.PL.Areas.DashBoard.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        [Area("DashBoard")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
